b5736f37d250c5403570749ef7869b4e578df671
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2024-03-30 17:45:13 +0800

    更新 miaojiexi.js

--- miaojiexi.js
+++ miaojiexi.js
@@ -19,16 +19,16 @@ hostname = tcb-api.tencentcloudapi.com
 
 
 //获取当前时间戳
-var currentTimeStamp = Date.当前();
+var currentTimeStamp = Date.now();
 //获取response_data响应
-var responseData = JSON.parse($response.body).data。response_data;
+var responseData = JSON.parse($response.body).data.response_data;
 //解析response_data中的data字段
 var data = JSON.parse(responseData).data;
 
 //修改数据
-data[0]。jieshushijian = 4092599349000;
-data[0]。kaitongshijian = currentTimeStamp;
-data[0]。qixian = "永久免除所有广告";
+data[0].jieshushijian = 4092599349000;
+data[0].kaitongshijian = currentTimeStamp;
+data[0].qixian = "永久免除所有广告";
 
 //更新response_data中的data字段
 responseData = JSON.stringify({ data: data });
@@ -36,6 +36,6 @@ responseData = JSON.stringify({ data: data });
 //更新原始数据
 var chxm1023 = JSON.parse($response.body);
 
-chxm1023。data.response_data = responseData;
+chxm1023.data.response_data = responseData;
 
 $done({ body: JSON.stringify(chxm1023) });
