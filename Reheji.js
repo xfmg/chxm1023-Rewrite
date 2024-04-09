0aee41ac18c9345ab03fbff9b8d3d0332c5157e9
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2024-04-09 13:56:42 +0800

    更新 Reheji.js

--- Reheji.js
+++ Reheji.js
@@ -2,7 +2,7 @@
 
 项目名称：Revenuecat 系列解锁合集
 下载地址：https://too.st/CollectionsAPP
-更新日期：2024-04-06
+更新日期：2024-04-09
 脚本作者：chxm1023
 电报频道：https://t.me/chxm1023
 使用声明：⚠️仅供参考，🈲转载与售卖！
@@ -45,6 +45,7 @@ const bundle = {
 
 //识别UA
 const list = {
+	'FoJiCam': { name: 'ProVersionLifeTime', id: 'com.uzero.cn.fojicam.life2', cm: 'sjb' },  //LimiCam-胶片相机
 	'FruitMinder': { name: 'premium', id: 'com.bartozo.FruitMinder.lifetime', cm: 'sjb' },  //FruitMinder-水果提醒
 	'PDF_convertor': { name: 'VIP', id: 'com.pdf.convertor.forever', cm: 'sjb' },  //PDF转换器
 	'rewritingText': { name: 'AIGrammercheckerProAccess', id: 'sv.aigrammerchecker.com.lifetime', cm: 'sjb' },  //AI Grammar
