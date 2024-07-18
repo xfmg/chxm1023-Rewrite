4df03acd17012689263cc24f971a93496d843d97
Yu9191 <132221547+Yu9191@users.noreply.github.com>
2024-07-18 21:39:34 +0800

    更新 iTunes.js

--- iTunes.js
+++ iTunes.js
@@ -27,6 +27,7 @@ const bundle_id = chxm1023.receipt["bundle_id"] || chxm1023.receipt["Bundle_Id"]
 
 const list = {
   'PICSPLAY': { cm: 'sjbla', hx: 'hxpda', id: "com.jellybus.PicsPlay2.IAP.PRO5999", latest: "chxm1023" },//PICSPLAY
+  'Rookie': { cm: 'sjbla', hx: 'hxpda', id: "com.jellybus.Rookie.IAP.PRO5999", latest: "chxm1023" },//RKCAM
   'MoneyWiz': { cm: 'sjbla', hx: 'hxpda', id: "com.moneywiz.personalfinance.1year", latest: "chxm1023" }, //MoneyWiz-个人财务
   'Overdrop': { cm: 'sjblb', hx: 'hxpda', id: "com.weather.overdrop.forever", latest: "chxm1023" }, //Overdrop-天气预报
   'qxzs': { cm: 'sjblb', hx: 'hxpda', id: "yongjiu", latest: "chxm1023" },//心率广播
