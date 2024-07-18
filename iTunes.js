4f4f00a53b59e2b0719c356df634659d4e2c9105
Yu9191 <132221547+Yu9191@users.noreply.github.com>
2024-07-18 22:12:25 +0800

    更新 iTunes.js

--- iTunes.js
+++ iTunes.js
@@ -26,6 +26,7 @@ const ua = $request.headers['User-Agent'] || $request.headers['user-agent'];
 const bundle_id = chxm1023.receipt["bundle_id"] || chxm1023.receipt["Bundle_Id"];
 
 const list = {
+  'VideoLab': { cm: 'sjbla', hx: 'hxpda', id: "com.jellybus.VideoLab.IAP.PRO7999", latest: "chxm1023" },//VideoLab
   'MOLDIV': { cm: 'sjbla', hx: 'hxpda', id: "com.jellybus.Moldiv.IAP.PRO7999", latest: "chxm1023" },//MOLDIV
   'PICSPLAY': { cm: 'sjbla', hx: 'hxpda', id: "com.jellybus.PicsPlay2.IAP.PRO5999", latest: "chxm1023" },//PICSPLAY
   'Rookie': { cm: 'sjbla', hx: 'hxpda', id: "com.jellybus.Rookie.IAP.PRO5999", latest: "chxm1023" },//RKCAM
